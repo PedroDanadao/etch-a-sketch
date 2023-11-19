2714631dc693de22c287e1009cf2748210914e6e
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-11-19 18:52:07 -0300

    Make the first version of the project
    This one ended up being quicker than I thought. I will implement the
    functionalities of the extra credits soon.

--- js/dist/script.js
+++ js/dist/script.js
@@ -1,12 +1,57 @@
 var containerDiv = document.querySelector(".container");
+var divsNumberButton = document.querySelector(".divs_number_button");
+divsNumberButton.addEventListener("click", changeDivsInRow);
 var divsInRow = 16;
-var containerWidth = 960;
+var CONTAINER_WIDTH = 960;
 var DIVS_GAP = 2;
-var divSize = Math.floor(containerWidth / divsInRow) - DIVS_GAP;
-for (var i = 0; i < divsInRow * divsInRow; i++) {
-    var newDiv = document.createElement("div");
-    newDiv.style["backgroundColor"] = "blue";
-    newDiv.style["width"] = divSize + "px";
-    newDiv.style["height"] = divSize + "px";
-    containerDiv.appendChild(newDiv);
+function makeDivs() {
+    /**
+     * creates the divs that will be inside the container
+     */
+    removeContainerDivs();
+    var divSize = CONTAINER_WIDTH / divsInRow - DIVS_GAP;
+    for (var i = 0; i < divsInRow * divsInRow; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.style["backgroundColor"] = "blue";
+        newDiv.style["width"] = divSize + "px";
+        newDiv.style["height"] = divSize + "px";
+        containerDiv.appendChild(newDiv);
+        newDiv.addEventListener("mouseover", changeDivColor);
+    }
 }
+function removeContainerDivs() {
+    /**
+     * Removes all the child divs from the container
+     */
+    while (containerDiv.firstChild) {
+        var lastChild = containerDiv.lastChild;
+        containerDiv.removeChild(lastChild);
+    }
+}
+function changeDivColor(event) {
+    /**
+     * changes the color of the div that has been hovered
+     */
+    var div = event.target;
+    div.style["backgroundColor"] = "red";
+}
+function changeDivsInRow() {
+    /**
+     * prompts the user to input the number of divs in a row and change the
+     * number of the divs that are in the container based on it
+     */
+    var numberOfDivs = Number(prompt("Insert number between 1 and 100:"));
+    if (isNaN(numberOfDivs) || numberOfDivs < 1) {
+        alert("The div number should be a valid positive integer");
+        return;
+    }
+    // make sure that the number will be a integer
+    numberOfDivs = Math.floor(numberOfDivs);
+    if (numberOfDivs > 100) {
+        alert("The div number you typed is above 100. FOr this scenario 100 divs in a row will be used");
+        numberOfDivs = 100;
+    }
+    divsInRow = numberOfDivs;
+    makeDivs();
+}
+makeDivs();
--- js/script.ts
+++ js/script.ts
@@ -1,17 +1,76 @@
 const containerDiv = document.querySelector(".container") as HTMLDivElement;
+const divsNumberButton = document.querySelector(".divs_number_button") as HTMLButtonElement;
 
-const divsInRow = 16;
-const containerWidth = 960;
+divsNumberButton.addEventListener("click", changeDivsInRow)
+
+let divsInRow = 16;
+const CONTAINER_WIDTH = 960;
 const DIVS_GAP = 2;
 
-const divSize = Math.floor(containerWidth / divsInRow) - DIVS_GAP
 
-for(let i=0; i < divsInRow * divsInRow; i++){
-    let newDiv = document.createElement("div") as HTMLElement;
+function makeDivs() {
+    /**
+     * creates the divs that will be inside the container
+     */
+    removeContainerDivs();
 
-    newDiv.style["backgroundColor"] = "blue";
-    newDiv.style["width"] = `${divSize}px`;
-    newDiv.style["height"] = `${divSize}px`;
-
-    containerDiv.appendChild(newDiv);
+    const divSize = CONTAINER_WIDTH / divsInRow - DIVS_GAP
+    
+    for(let i=0; i < divsInRow * divsInRow; i++){
+        let newDiv = document.createElement("div") as HTMLElement;
+    
+        newDiv.style["backgroundColor"] = "blue";
+        newDiv.style["width"] = `${divSize}px`;
+        newDiv.style["height"] = `${divSize}px`;
+    
+        containerDiv.appendChild(newDiv);
+    
+        newDiv.addEventListener("mouseover", changeDivColor);
+    }
+    
 }
+
+function removeContainerDivs() {
+    /**
+     * Removes all the child divs from the container
+     */
+    while(containerDiv.firstChild) {
+        let lastChild = containerDiv.lastChild as HTMLDivElement;
+        containerDiv.removeChild(lastChild);
+    }
+}
+
+function changeDivColor(event: Event) {
+    /**
+     * changes the color of the div that has been hovered
+     */
+    const div = event.target as HTMLDivElement;
+
+    div.style["backgroundColor"] = "red";
+}
+
+function changeDivsInRow() {
+    /**
+     * prompts the user to input the number of divs in a row and change the 
+     * number of the divs that are in the container based on it
+     */
+    let numberOfDivs = Number(prompt("Insert number between 1 and 100:"));
+
+    if(isNaN(numberOfDivs) || numberOfDivs < 1){
+        alert("The div number should be a valid positive integer");
+        return
+    }
+
+    // make sure that the number will be a integer
+    numberOfDivs = Math.floor(numberOfDivs);
+
+    if(numberOfDivs > 100) {
+        alert("The div number you typed is above 100. FOr this scenario 100 divs in a row will be used");
+        numberOfDivs = 100;
+    }
+
+    divsInRow = numberOfDivs;
+    makeDivs();
+}
+
+makeDivs();
