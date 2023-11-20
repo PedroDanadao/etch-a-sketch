8a1c89f5da2e37fd70cee45c0edc0503e625b913
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2023-11-20 11:44:18 -0300

    Make extra credits
    Made the functionalities that where specified in the extra credits
    section of the project, them being, assigning a random color to the
    hovered div and make each new color 10% darker whenever the user hovers
    the mouse over a previously random colored div.

--- js/dist/script.js
+++ js/dist/script.js
@@ -12,9 +12,12 @@ function makeDivs() {
     var divSize = CONTAINER_WIDTH / divsInRow - DIVS_GAP;
     for (var i = 0; i < divsInRow * divsInRow; i++) {
         var newDiv = document.createElement("div");
-        newDiv.style["backgroundColor"] = "blue";
+        newDiv.style["background-color"] = "blue";
         newDiv.style["width"] = divSize + "px";
         newDiv.style["height"] = divSize + "px";
+        // create a attribute in the new div to check if it's color has already 
+        // been changed and to check the color that has been changed
+        newDiv.setAttribute("changed_to_color", '');
         containerDiv.appendChild(newDiv);
         newDiv.addEventListener("mouseover", changeDivColor);
     }
@@ -33,7 +36,51 @@ function changeDivColor(event) {
      * changes the color of the div that has been hovered
      */
     var div = event.target;
-    div.style["backgroundColor"] = "red";
+    if (!div.getAttribute("changed_to_color")) {
+        return assignNewDivColor(div);
+    }
+    darkenDivColor(div);
+}
+function assignNewDivColor(div) {
+    var randomColorValues = getRandomColor();
+    var rgbString = "rgb(" + randomColorValues[0] + ", " + randomColorValues[1] + ", " + randomColorValues[2] + ")";
+    var attributeString = randomColorValues[0] + " " + randomColorValues[1] + " " + randomColorValues[2];
+    div.style["backgroundColor"] = rgbString;
+    div.setAttribute("changed_to_color", attributeString);
+}
+function darkenDivColor(div) {
+    var divOriginalColorString = div.getAttribute("changed_to_color");
+    var colorOriginalRGBValues = divOriginalColorString.split(' ');
+    var currentRGBString = div.style["backgroundColor"];
+    // if the current RGB of the div is already 0, 0, 0 then there is no need to 
+    // run the rest of the function
+    if (currentRGBString === "rgb(0, 0, 0)")
+        return;
+    currentRGBString = currentRGBString.replace("rgb(", '');
+    currentRGBString = currentRGBString.replace(')', '');
+    var colorCurrentRGBValues = currentRGBString.split(", ");
+    var darkerRGBNumbers = [];
+    for (var i = 0; i < 3; i++) {
+        var originalString = colorOriginalRGBValues[i];
+        var currentString = colorCurrentRGBValues[i];
+        var originalNumber = Number(originalString);
+        var originalTenth = Math.max(originalNumber / 10, 1);
+        var currentNumber = Number(currentString);
+        var darkerNumber = currentNumber - originalTenth;
+        darkerNumber = Math.max(Math.floor(darkerNumber), 0);
+        darkerRGBNumbers.push(darkerNumber);
+    }
+    var newDivColor = "rgb(" + darkerRGBNumbers[0] + ", " + darkerRGBNumbers[1] + ", " + darkerRGBNumbers[2] + ")";
+    div.style["backgroundColor"] = newDivColor;
+}
+function getRandomColor() {
+    /**
+     * returns a random hexadecimal value representing a color
+     */
+    var r_value = Math.floor(Math.random() * 256);
+    var g_value = Math.floor(Math.random() * 256);
+    var b_value = Math.floor(Math.random() * 256);
+    return [r_value, g_value, b_value];
 }
 function changeDivsInRow() {
     /**
--- js/script.ts
+++ js/script.ts
@@ -19,9 +19,13 @@ function makeDivs() {
     for(let i=0; i < divsInRow * divsInRow; i++){
         let newDiv = document.createElement("div") as HTMLElement;
     
-        newDiv.style["backgroundColor"] = "blue";
+        newDiv.style["background-color"] = "blue";
         newDiv.style["width"] = `${divSize}px`;
         newDiv.style["height"] = `${divSize}px`;
+
+        // create a attribute in the new div to check if it's color has already 
+        // been changed and to check the color that has been changed
+        newDiv.setAttribute("changed_to_color", '');
     
         containerDiv.appendChild(newDiv);
     
@@ -46,7 +50,64 @@ function changeDivColor(event: Event) {
      */
     const div = event.target as HTMLDivElement;
 
-    div.style["backgroundColor"] = "red";
+    if(!div.getAttribute("changed_to_color")){
+        return assignNewDivColor(div);
+    }
+
+    darkenDivColor(div);
+}
+
+function assignNewDivColor(div: HTMLDivElement) {
+    const randomColorValues = getRandomColor();
+    const rgbString = `rgb(${randomColorValues[0]}, ${randomColorValues[1]}, ${randomColorValues[2]})`;
+    const attributeString = `${randomColorValues[0]} ${randomColorValues[1]} ${randomColorValues[2]}`;
+
+    div.style["backgroundColor"] = rgbString;
+    div.setAttribute("changed_to_color", attributeString);
+}
+
+function darkenDivColor(div: HTMLDivElement) {
+    const divOriginalColorString = div.getAttribute("changed_to_color") as string;
+    const colorOriginalRGBValues = divOriginalColorString.split(' ');
+
+    let currentRGBString = div.style["backgroundColor"];
+    // if the current RGB of the div is already 0, 0, 0 then there is no need to 
+    // run the rest of the function
+    if (currentRGBString === "rgb(0, 0, 0)") return
+    
+    currentRGBString = currentRGBString.replace("rgb(", '');
+    currentRGBString = currentRGBString.replace(')', '');
+    const colorCurrentRGBValues = currentRGBString.split(", ");
+
+    const darkerRGBNumbers : Number[] = [];
+
+    for(let i=0; i < 3; i++){
+        let originalString = colorOriginalRGBValues[i];
+        let currentString = colorCurrentRGBValues[i];
+
+        let originalNumber = Number(originalString);
+        let originalTenth = Math.max( originalNumber / 10, 1 );
+
+        let currentNumber = Number(currentString);
+        let darkerNumber = currentNumber - originalTenth;
+        darkerNumber = Math.max( Math.floor(darkerNumber), 0 );
+
+        darkerRGBNumbers.push(darkerNumber);
+    }
+
+    const newDivColor = `rgb(${darkerRGBNumbers[0]}, ${darkerRGBNumbers[1]}, ${darkerRGBNumbers[2]})`;
+    div.style["backgroundColor"] = newDivColor;
+}
+
+function getRandomColor() {
+    /**
+     * returns a random hexadecimal value representing a color
+     */
+    const r_value = Math.floor( Math.random() * 256 );
+    const g_value = Math.floor( Math.random() * 256 );
+    const b_value = Math.floor( Math.random() * 256 );
+
+    return [r_value, g_value, b_value];
 }
 
 function changeDivsInRow() {
